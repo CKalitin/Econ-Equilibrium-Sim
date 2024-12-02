2b21a1705f31a4eb91e917132ce72628767a1430
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2024-12-01 19:47:04 -0800

    basic agent and consumed goods

---
+++ Project/goods.py
@@ -0,0 +1,30 @@
+# This file contains goods classes and specific good types (eg. food, house)
+
+# These goods are consumed by agents and bought and sold in the market
+# They are not exchanged as instances, only quantitatively - eg. if you buy 5 food, your food quantity goes up by 5
+class good_consumed():
+    def __init__(self, price):
+        pass
+
+
+# Allocated goods are exchanged by reference, eg. if you buy a house, you get a house object
+# Jobs use allocated goods, hence the owner agent and consumer agent variables, these are references to the owner and agent that are useful when buying and selling
+class good_allocated():
+    def __init__(self, price):
+        self.owner_agent = None
+        self.consumer_agent = None
+        self.price = price
+
+
+class good_consumed_marginal_value_function():
+    def __init__(self, income_coeffs):
+        # This is a polynomials ax^2 + bx + c, the array goes [a, b, c]
+        self.income_coeffs = income_coeffs
+    
+    def get_marginal_value(self, quantity):
+        return self.income_coeffs[0] ** quantity + self.income_coeffs[1] * quantity + self.income_coeffs[2]
+    
+
+good_marginal_value_functions = {}
+good_marginal_value_functions["food"] = good_consumed_marginal_value_function([0, -0.5, 2])
+good_marginal_value_functions["house"] = good_consumed_marginal_value_function([0, 1, 1])
